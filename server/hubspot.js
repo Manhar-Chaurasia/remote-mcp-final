268e0683c0ddfb24a738fdc1dc4e35630f18e609
manhar <manharchaurasia100@gmail.com>
2025-11-17 16:15:49 +0530

    env

--- server/hubspot.js
+++ server/hubspot.js
@@ -1,6 +1,6 @@
 import HubSpot from "@hubspot/api-client";
 
-const accessToken = process.env.HUBSPOT_PRIVATE_TOKEN;
+const accessToken = "pat-na1-43b0ac76-bb60-4053-9369-17f3517f87b7";
 
 if (!accessToken) {
   throw new Error("Missing HUBSPOT_PRIVATE_TOKEN in environment variables");
